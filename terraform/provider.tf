f4beab4ecd07eed0e417b6c8a83b8255e5f82d01
Jake Watkins <jwatkins@mozilla.com>
2019-02-12 12:40:08 -0800

    Add aws providers and shared vars

---
+++ terraform/provider.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    version = "~> 1.58"
+    allowed_account_ids = [ "961225894672" ]
+    region = "${var.region}"
+}
+provider "aws" {
+    version = "~> 1.58"
+    allowed_account_ids = [ "961225894672" ]
+    alias = "us-east-1"
+    region = "us-east-1"
+}
+provider "aws" {
+    version = "~> 1.58"
+    allowed_account_ids = [ "961225894672" ]
+    alias = "us-west-2"
+    region = "us-west-2"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+# This file contains empty declarations for variables that will be definied in each
+# environments terraform.tfvars file
+
+variable "region" {
+    description = "The AWS region to create things in."
+    default = "us-west-2"
+}
