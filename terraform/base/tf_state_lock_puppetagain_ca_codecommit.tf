1347bc7c630931b3f1d3108d77266d6c37e89148
Jake Watkins <jwatkins@mozilla.com>
2019-02-13 13:45:55 -0800

    Separate dynamodb state locking resources

--- terraform/base/tf_state_lock_puppetagain_ca_codecommit.tf
+++ terraform/base/tf_state_lock_puppetagain_ca_codecommit.tf
@@ -1,23 +1,3 @@
-resource "aws_dynamodb_table" "tf_state_lock_base" {
-  name           = "tf_state_lock_base"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags {
-    Name        = "Base Terraform State Lock Table"
-    Terraform   = "true"
-    Repo_url    = "${var.repo_url}"
-    Environment = "prod"
-    Owner       = "relops@mozilla.com"
-  }
-}
-
 resource "aws_dynamodb_table" "tf_state_lock_puppetagain_ca_codecommit" {
   name           = "tf_state_lock_puppetagain_ca_codecommit"
   hash_key       = "LockID"
