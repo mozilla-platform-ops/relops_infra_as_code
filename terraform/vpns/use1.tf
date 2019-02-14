2a48dd3773b356e19685022e465f79083159fd52
Jake Watkins <jwatkins@mozilla.com>
2019-02-13 18:55:38 -0800

    VPN resources establish connections to use1/usw2 <-> mdc1/2

---
+++ terraform/vpns/use1.tf
@@ -0,0 +1,99 @@
+resource "aws_vpn_gateway" "vpn_gw_use1" {
+  provider = "aws.us-east-1"
+
+  tags {
+    Name        = "USE1 VPN Gateway"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_customer_gateway" "cgw_use1_mdc1" {
+  provider   = "aws.us-east-1"
+  bgp_asn    = 65048
+  ip_address = "63.245.208.251"
+  type       = "ipsec.1"
+
+  tags {
+    Name        = "MDC1 Customer Gateway"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_customer_gateway" "cgw_use1_mdc2" {
+  provider   = "aws.us-east-1"
+  bgp_asn    = 65050
+  ip_address = "63.245.210.251"
+  type       = "ipsec.1"
+
+  tags {
+    Name        = "MDC2 Customer Gateway"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_vpn_connection" "vpn_connection_use1_mdc1" {
+  provider            = "aws.us-east-1"
+  vpn_gateway_id      = "${aws_vpn_gateway.vpn_gw_use1.id}"
+  customer_gateway_id = "${aws_customer_gateway.cgw_use1_mdc1.id}"
+  type                = "ipsec.1"
+
+  tags {
+    Name        = "USE1-MDC1"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_vpn_connection" "vpn_connection_use1_mdc2" {
+  provider            = "aws.us-east-1"
+  vpn_gateway_id      = "${aws_vpn_gateway.vpn_gw_use1.id}"
+  customer_gateway_id = "${aws_customer_gateway.cgw_use1_mdc2.id}"
+  type                = "ipsec.1"
+
+  tags {
+    Name        = "USE1-MDC2"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+data "aws_vpcs" "moz_internal_us_east_1" {
+  provider = "aws.us-east-1"
+
+  tags = {
+    Name = "moz-internal-us-east-1"
+  }
+}
+
+resource "aws_vpn_gateway_attachment" "vpn_attachment_use1" {
+  provider       = "aws.us-east-1"
+  vpc_id         = "${data.aws_vpcs.moz_internal_us_east_1.ids[0]}"
+  vpn_gateway_id = "${aws_vpn_gateway.vpn_gw_use1.id}"
+}
+
+data "aws_route_tables" "moz_internal_us_east_1_public" {
+  provider = "aws.us-east-1"
+
+  tags = {
+    Name = "moz-internal-us-east-1-public"
+  }
+}
+
+resource "aws_vpn_gateway_route_propagation" "public_use1" {
+  provider       = "aws.us-east-1"
+  route_table_id = "${data.aws_route_tables.moz_internal_us_east_1_public.ids[0]}"
+  vpn_gateway_id = "${aws_vpn_gateway.vpn_gw_use1.id}"
+}
---
+++ terraform/vpns/usw2.tf
@@ -0,0 +1,99 @@
+resource "aws_vpn_gateway" "vpn_gw_usw2" {
+  provider = "aws.us-west-2"
+
+  tags {
+    Name        = "USW1 VPN Gateway"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_customer_gateway" "cgw_usw2_mdc1" {
+  provider   = "aws.us-west-2"
+  bgp_asn    = 65048
+  ip_address = "63.245.208.251"
+  type       = "ipsec.1"
+
+  tags {
+    Name        = "MDC1 Customer Gateway"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_customer_gateway" "cgw_usw2_mdc2" {
+  provider   = "aws.us-west-2"
+  bgp_asn    = 65050
+  ip_address = "63.245.210.251"
+  type       = "ipsec.1"
+
+  tags {
+    Name        = "MDC2 Customer Gateway"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_vpn_connection" "vpn_connection_usw2_mdc1" {
+  provider            = "aws.us-west-2"
+  vpn_gateway_id      = "${aws_vpn_gateway.vpn_gw_usw2.id}"
+  customer_gateway_id = "${aws_customer_gateway.cgw_usw2_mdc1.id}"
+  type                = "ipsec.1"
+
+  tags {
+    Name        = "USW2-MDC1"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_vpn_connection" "vpn_connection_usw2_mdc2" {
+  provider            = "aws.us-west-2"
+  vpn_gateway_id      = "${aws_vpn_gateway.vpn_gw_usw2.id}"
+  customer_gateway_id = "${aws_customer_gateway.cgw_usw2_mdc2.id}"
+  type                = "ipsec.1"
+
+  tags {
+    Name        = "USW2-MDC2"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
+
+data "aws_vpcs" "moz_internal_us_west_2" {
+  provider = "aws.us-west-2"
+
+  tags = {
+    Name = "moz-internal-us-west-2"
+  }
+}
+
+resource "aws_vpn_gateway_attachment" "vpn_attachment_usw2" {
+  provider       = "aws.us-west-2"
+  vpc_id         = "${data.aws_vpcs.moz_internal_us_west_2.ids[0]}"
+  vpn_gateway_id = "${aws_vpn_gateway.vpn_gw_usw2.id}"
+}
+
+data "aws_route_tables" "moz_internal_us_west_2_public" {
+  provider = "aws.us-west-2"
+
+  tags = {
+    Name = "moz-internal-us-west-2-public"
+  }
+}
+
+resource "aws_vpn_gateway_route_propagation" "public_usw2" {
+  provider       = "aws.us-west-2"
+  route_table_id = "${data.aws_route_tables.moz_internal_us_west_2_public.ids[0]}"
+  vpn_gateway_id = "${aws_vpn_gateway.vpn_gw_usw2.id}"
+}
