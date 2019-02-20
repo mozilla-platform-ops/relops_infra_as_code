95f05c1258e28779cffc9f39da976ee5b771f0d3
Jake Watkins <jwatkins@mozilla.com>
2019-02-20 14:23:40 -0800

    Create route53 zones and fully populate usw2

--- terraform/base/vpc.tf
+++ terraform/base/vpc.tf
@@ -15,15 +15,35 @@ module "vpc_moz_internal_us_east_1" {
     Subnet_type = "public"
   }
 
-  enable_dns_hostnames    = true
-  enable_dns_support      = true
-  map_public_ip_on_launch = false
+  enable_dhcp_options      = true
+  dhcp_options_domain_name = "use1.relops"
+  enable_dns_hostnames     = true
+  enable_dns_support       = true
+  map_public_ip_on_launch  = false
 
   tags = {
     Terraform   = "true"
     Repo_url    = "${var.repo_url}"
     Environment = "Prod"
     Owner       = "relops@mozilla.com"
+  }
+}
+
+resource "aws_route53_zone" "use1" {
+  name = "use1.relops"
+
+  vpc {
+    vpc_id     = "${module.vpc_moz_internal_us_east_1.vpc_id}"
+    vpc_region = "us-east-1"
+  }
+}
+
+resource "aws_route53_zone" "use1_arpa" {
+  name = "6.191.10.in-addr.arpa."
+
+  vpc {
+    vpc_id     = "${module.vpc_moz_internal_us_east_1.vpc_id}"
+    vpc_region = "us-east-1"
   }
 }
 
@@ -40,9 +60,17 @@ module "vpc_moz_internal_us_west_2" {
   azs            = ["us-west-2a", "us-west-2b"]
   public_subnets = ["10.191.7.0/25", "10.191.7.128/25"]
 
-  enable_dns_hostnames    = true
-  enable_dns_support      = true
-  map_public_ip_on_launch = false
+  public_subnet_tags = {
+    Subnet_type = "public"
+  }
+
+  enable_dhcp_options      = true
+  dhcp_options_domain_name = "usw2.relops"
+  enable_dns_hostnames     = true
+  enable_dns_support       = true
+  map_public_ip_on_launch  = false
+
+  enable_dynamodb_endpoint = true
 
   tags = {
     Terraform   = "true"
@@ -50,4 +78,44 @@ module "vpc_moz_internal_us_west_2" {
     Environment = "prod"
     Owner       = "relops@mozilla.com"
   }
+}
+
+resource "aws_route53_zone" "usw2" {
+  name = "usw2.relops"
+
+  vpc {
+    vpc_id     = "${module.vpc_moz_internal_us_west_2.vpc_id}"
+    vpc_region = "us-west-2"
+  }
+}
+
+resource "aws_route53_zone" "usw2_arpa" {
+  name = "7.191.10.in-addr.arpa."
+
+  vpc {
+    vpc_id     = "${module.vpc_moz_internal_us_west_2.vpc_id}"
+    vpc_region = "us-west-2"
+  }
+}
+
+resource "aws_route53_record" "populate_usw2_dns" {
+  zone_id = "${aws_route53_zone.usw2.zone_id}"
+
+  count = 254
+
+  name    = "${format("ip-10-191-7-%d", count.index + 1)}"
+  type    = "A"
+  ttl     = "300"
+  records = ["${format("10.191.7.%d", count.index + 1)}"]
+}
+
+resource "aws_route53_record" "populate_usw2_rdns" {
+  zone_id = "${aws_route53_zone.usw2_arpa.zone_id}"
+
+  count = 254
+
+  name    = "${format("%d.7.191.10.in-addr.arpa.", count.index + 1)}"
+  type    = "PTR"
+  ttl     = "300"
+  records = ["${format("ip-10-191-7-%d.usw2.relops", count.index + 1)}"]
 }
