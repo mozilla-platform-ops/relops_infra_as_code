16b024b401b349806254b98d662fa08c0117b02e
Andrew Erickson <aerickson@mozilla.com>
2019-02-21 17:15:12 -0800

    tweak name

--- terraform/taskqueue-influxdb-metrics/cloudwatch.tf
+++ terraform/taskqueue-influxdb-metrics/cloudwatch.tf
@@ -7,6 +7,6 @@ resource "aws_cloudwatch_event_rule" "1m" {
 
 resource "aws_cloudwatch_event_target" "lambda_trigger" {
   rule      = "${aws_cloudwatch_event_rule.1m.name}"
-  target_id = "Lambda Trigger"
+  target_id = "lambda_trigger"
   arn       = "${aws_lambda_function.log_bitbar_data_to_influx.arn}"
 }
