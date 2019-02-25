c0fa6a9695019696cefba1277d8fbbb07d565cba
Jake Watkins <jwatkins@mozilla.com>
2019-02-25 14:27:33 -0800

    Hosted zone for relops.mozops.net

---
+++ terraform/base/route53.tf
@@ -0,0 +1,5 @@
+# This is a hosted zone delegated from mozops.net
+# which is managed under the moz-devservices aws account
+resource "aws_route53_zone" "relops_mozops_net_public" {
+  name = "relops.mozops.net"
+}
