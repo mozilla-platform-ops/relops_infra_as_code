bd0d1caef77887cd9332e4ca221bb92066429bd2
Jake Watkins <jwatkins@mozilla.com>
2019-03-04 15:32:57 -0800

    Send aws cloudtrail logs to firefox security s3 bucket

---
+++ terraform/base/cloudtrail.tf
@@ -0,0 +1,14 @@
+# Creates a cloudtrail for sending aws logs to firefox security for auditing
+
+resource "aws_cloudtrail" "cloudtrail" {
+  name                          = "cloudtrail-to-foxsec"
+  s3_bucket_name                = "moz-cloudtrail-logs"
+  s3_key_prefix                 = "relops-aws-prod"
+  include_global_service_events = true
+  is_multi_region_trail         = true
+
+  tags {
+    Name = "cloudtrail-to-foxsec"
+    bug  = "1531162"
+  }
+}
