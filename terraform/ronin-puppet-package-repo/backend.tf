8bf075d7e28f729b58c255ad6a9dec2bc03266c3
Jake Watkins <jwatkins@mozilla.com>
2019-03-12 08:34:23 -0700

    Initial state for ronin puppet package repo

---
+++ terraform/ronin-puppet-package-repo/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "relops-tf-states"
+    key            = "ronin-puppet-package-repo.tfstate"
+    dynamodb_table = "tf_state_lock_ronin-puppet-package-repo"
+    region         = "us-west-2"
+  }
+}
---
+++ terraform/ronin-puppet-package-repo/terraform.tfvars
@@ -0,0 +1,1 @@
+tag_project_name = "ronin-puppet-package-repo"
