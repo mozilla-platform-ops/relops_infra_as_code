d2c11b6cfc0ce078056ad1c95565524f1ee462c1
Dave House <dhouse@mozilla.com>
2019-04-02 15:34:28 -0600

    add telegraf vars

--- terraform/telegraf/terraform.tfvars
+++ terraform/telegraf/terraform.tfvars
@@ -4,8 +4,20 @@ app_count = 1
 
 app_image = "961225894672.dkr.ecr.us-west-2.amazonaws.com/telegraf:latest"
 
-fargate_cpu = 512
+fargate_cpu = 4096
 
-fargate_memory = 1024
+fargate_memory = 8192
 
 app_port = 8086
+
+influxdb_url = "http://34.216.9.122:8086"
+
+influxdb_db = "relops"
+
+influxdb_user = "relops_wo"
+
+interval = "150s"
+
+medium_interval = "300s"
+
+long_interval = "600s"
--- terraform/telegraf/vars.tf
+++ terraform/telegraf/vars.tf
@@ -17,3 +17,33 @@ variable "fargate_memory" {
 variable "app_port" {
   description = "Port number of application"
 }
+
+variable "influxdb_url" {
+  description = "InfluxDB host url"
+  default     = "http://influxdb:8086"
+}
+
+variable "influxdb_db" {
+  description = "InfluxDB database name"
+  default     = "default"
+}
+
+variable "influxdb_user" {
+  description = "InfluxDB user name"
+  default     = "admin"
+}
+
+variable "interval" {
+  description = "Metric collection default interval"
+  default     = "60s"
+}
+
+variable "medium_interval" {
+  description = "Metric collection medium interval"
+  default     = "300s"
+}
+
+variable "long_interval" {
+  description = "Metric collection long interval"
+  default     = "600s"
+}
