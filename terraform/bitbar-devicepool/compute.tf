4432354f466b740b6332b3c4894329e3a6542576
Andrew Erickson <aerickson@mozilla.com>
2019-04-29 13:55:55 -0700

    get ssh working

--- terraform/bitbar-devicepool/compute.tf
+++ terraform/bitbar-devicepool/compute.tf
@@ -4,6 +4,11 @@ resource "google_compute_instance" "vm_instance" {
   name         = "bitbar-devicepool-${count.index}"
   machine_type = "f1-micro"
 
+  metadata {
+    // can take multiple, just separate with "\n"
+    sshKeys = "${var.ssh_user}:${var.ssh_key}"
+  }
+
   boot_disk {
     initialize_params {
       image = "ubuntu-1804-lts"
@@ -12,7 +17,27 @@ resource "google_compute_instance" "vm_instance" {
 
   network_interface {
     # A default network is created for all GCP projects
-    network       = "default"
+    network       = "${google_compute_network.bitbar-net.name}"
     access_config = {}
   }
+}
+
+resource "google_compute_firewall" "devicepool-firewall" {
+  name    = "devicepool-firewall"
+  network = "${google_compute_network.bitbar-net.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_network" "bitbar-net" {
+  name = "bitbar-net"
 }
