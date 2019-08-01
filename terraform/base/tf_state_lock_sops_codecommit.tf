028ea2f0586ce0b683d6e77b976bdde422e9999f
Jake Watkins <jwatkins@mozilla.com>
2019-08-01 10:56:33 -0700

    New relops sops code commit repo

---
+++ terraform/base/tf_state_lock_sops_codecommit.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "tf_state_lock_sops_codecommit" {
+  name           = "tf_state_lock_sops_codecommit"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "sops_codecommit Terraform State Lock Table"
+    Terraform   = "true"
+    Repo_url    = "${var.repo_url}"
+    Environment = "prod"
+    Owner       = "relops@mozilla.com"
+  }
+}
---
+++ terraform/sops_codecommit/terraform.tfvars
@@ -0,0 +1,6 @@
+tag_project_name = "sops_codecommit"
+
+tag_production_state = "production"
+
+tag_owner_email = "jwatkins@mozilla.com"
+
