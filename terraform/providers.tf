0e734933141392432712c823138cc3c0e3868a5d
Jake Watkins <jwatkins@mozilla.com>
2019-07-31 17:18:55 -0700

    update aws provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  version             = "~> 2.17"
+  version             = "~> 2.0"
   allowed_account_ids = ["961225894672"]
   region              = "${var.region}"
 }
 
 provider "aws" {
-  version             = "~> 2.17"
+  version             = "~> 2.0"
   allowed_account_ids = ["961225894672"]
   alias               = "us-east-1"
   region              = "us-east-1"
 }
 
 provider "aws" {
-  version             = "~> 2.17"
+  version             = "~> 2.0"
   allowed_account_ids = ["961225894672"]
   alias               = "us-west-2"
   region              = "us-west-2"
