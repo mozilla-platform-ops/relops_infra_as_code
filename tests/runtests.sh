6fdd02577c582d05ca8ebcc43846ec8a4e54701a
Andrew Erickson <aerickson@mozilla.com>
2019-08-14 12:58:51 -0700

    try to fix up tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -4,6 +4,7 @@
 # https://github.com/travis-infrastructure/terraform-config/blob/master/runtests
 
 set -euo pipefail
+set -x
 
 main() {
   echo -e '\n-----> Validating JSON'
@@ -21,7 +22,10 @@ main() {
   done
 
   echo -e '\n-----> Running terraform validate'
+  # setup
+  ../terraform/taskqueue-influxdb-metrics/create_zip.sh
   export TF_IN_AUTOMATION=true
+  # test
   for d in $(git ls-files '*.tf' | xargs -n1 dirname | LC_ALL=C sort | grep -E -v '^\.$|^terraform$|^terraform/bitbar-devicepool$' | uniq); do
     echo -en "${d} "
     cd "${d}" || exit 1
