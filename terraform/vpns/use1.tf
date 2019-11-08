5e6e3a145334041085bac5c098f0950b1f8d0dc5
Jake Watkins <jwatkins@mozilla.com>
2019-11-08 09:07:20 -0800

    Revert macstadium vpn and vault lb changes

--- terraform/vpns/use1.tf
+++ terraform/vpns/use1.tf
@@ -40,22 +40,6 @@ resource "aws_customer_gateway" "cgw_use1_mdc2" {
   }
 }
 
-resource "aws_customer_gateway" "cgw_use1_macstadium_las_vegas" {
-  provider   = "aws.us-east-1"
-  bgp_asn    = 65000
-  ip_address = "207.254.35.84"
-  type       = "ipsec.1"
-
-  tags = {
-    Name        = "MacStadium Las Vegas Customer Gateway"
-    Terraform   = "true"
-    Repo_url    = "${var.repo_url}"
-    Environment = "prod"
-    Owner       = "relops@mozilla.com"
-  }
-}
-
-
 resource "aws_vpn_connection" "vpn_connection_use1_mdc1" {
   provider            = "aws.us-east-1"
   vpn_gateway_id      = "${aws_vpn_gateway.vpn_gw_use1.id}"
@@ -84,28 +68,6 @@ resource "aws_vpn_connection" "vpn_connection_use1_mdc2" {
     Environment = "prod"
     Owner       = "relops@mozilla.com"
   }
-}
-
-resource "aws_vpn_connection" "vpn_connection_use1_macstadium_las_vegas" {
-  provider            = "aws.us-east-1"
-  vpn_gateway_id      = aws_vpn_gateway.vpn_gw_use1.id
-  customer_gateway_id = aws_customer_gateway.cgw_use1_macstadium_las_vegas.id
-  type                = "ipsec.1"
-  static_routes_only  = true
-
-  tags = {
-    Name        = "USE1-MACSTADIUM"
-    Terraform   = "true"
-    Repo_url    = "${var.repo_url}"
-    Environment = "prod"
-    Owner       = "relops@mozilla.com"
-  }
-}
-
-resource "aws_vpn_connection_route" "use1_macstadium_las_vegas_route" {
-  provider               = "aws.us-east-1"
-  destination_cidr_block = "10.10.10.0/24"
-  vpn_connection_id      = aws_vpn_connection.vpn_connection_use1_macstadium_las_vegas.id
 }
 
 data "aws_vpcs" "moz_internal_us_east_1" {
