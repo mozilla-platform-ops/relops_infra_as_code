a0efb8350e8cad3966c305eca58d4628e8cc2029
davehouse <dhouse@mozilla.com>
2020-02-19 12:46:18 -0700

    remove terraform/grafana
    completed destroy of artifacts

--- terraform/grafana/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "relops-tf-states"
-    key            = "grafana.tfstate"
-    dynamodb_table = "tf_state_lock_grafana"
-    region         = "us-west-2"
-  }
-}
