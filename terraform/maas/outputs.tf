12ae6c1c0273bda06e4acaed00e80016e716d893
Jake Watkins <jwatkins@mozilla.com>
2020-04-30 18:17:00 -0700

    [maas] add mac address as outputs

---
+++ terraform/maas/outputs.tf
@@ -0,0 +1,7 @@
+output "mdc1_regional_1_mac_address" {
+  value = vsphere_virtual_machine.mdc1_wintest_rackd_1.network_interface.0.mac_address
+}
+
+output "mdc2_regional_1_mdc2_mac_address" {
+  value = vsphere_virtual_machine.mdc2_wintest_rackd_1.network_interface.0.mac_address
+}
