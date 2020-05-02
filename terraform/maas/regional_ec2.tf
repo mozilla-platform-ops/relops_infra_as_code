79247338ce560bb12d7aa924afcaf988914b38ff
Jake Watkins <jwatkins@mozilla.com>
2020-05-01 18:29:44 -0700

    [maas] AWS regional controller and postgres rds
            This commit introduces the aws bits for the maas regional
            contorller.  It includes 1 ec2 instance, a load balancer for
            proxying https to maas, and a m4.large rds postgres instance
            with minimal spec.  The specs can be increased as needed.  We
            also introduce a endpoint URL for the lb with an AWS managed ssl
            certificate attached to the load balancer.
    
                service URL: https://maas.relops.mozops.net/MAAS/

---
+++ terraform/maas/regional_ec2.tf
@@ -0,0 +1,73 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "regional_1" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.large"
+  vpc_security_group_ids      = [aws_security_group.maas_ec2_sg.id]
+  key_name                    = "dividehex"
+  subnet_id                   = "subnet-0f7d7eedf80f0506b"
+  associate_public_ip_address = true
+
+  tags = merge(
+    local.common_tags,
+    map(
+      "Name", "maas-regional-1"
+    )
+  )
+}
+
+resource "aws_security_group" "maas_ec2_sg" {
+  name        = "maas-regional-sg"
+  description = "Allow maas traffic into ec2 instances"
+  vpc_id      = join(", ", data.aws_vpcs.moz_internal_us_west_2.ids)
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 5240
+    to_port         = 5240
+    protocol        = "tcp"
+    security_groups = [aws_security_group.maas_lb_sg.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.49.0.0/16", "10.51.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    map(
+      "Name", "maas-regional-sg"
+    )
+  )
+}
+
