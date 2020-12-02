9f808d08f9498b36d0ad7eb3812a384c2ee48bd7
Jake Watkins <jwatkins@mozilla.com>
2020-12-01 21:44:40 -0800

    Bring taskqueue-influxdb-metrics tf into 0.12 compliance

--- terraform/taskqueue-influxdb-metrics/lambda.tf
+++ terraform/taskqueue-influxdb-metrics/lambda.tf
@@ -18,8 +18,8 @@ EOF
 
 resource "aws_iam_policy_attachment" "secretsmanager_to_influx" {
   name       = "secretsmanager_to_influx"
-  roles      = ["${aws_iam_role.iam_for_lambda.name}"]
-  policy_arn = "${aws_iam_policy.secretsmanager-influx.arn}"
+  roles      = [aws_iam_role.iam_for_lambda.name]
+  policy_arn = aws_iam_policy.secretsmanager-influx.arn
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
@@ -47,7 +47,7 @@ EOF
 resource "aws_lambda_function" "bitbar_influx_logger" {
   filename         = "function.zip"
   function_name    = "bitbar_influx_logger"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  role             = aws_iam_role.iam_for_lambda.arn
   handler          = "function.lambda_handler"
   source_code_hash = filebase64sha256("function.zip")
   runtime          = "python3.6"
@@ -58,7 +58,7 @@ resource "aws_lambda_function" "bitbar_influx_logger" {
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.bitbar_influx_logger.function_name}"
+  function_name = aws_lambda_function.bitbar_influx_logger.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.tick_1m.arn}"
+  source_arn    = aws_cloudwatch_event_rule.tick_1m.arn
 }
