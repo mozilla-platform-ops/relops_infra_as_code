3b35178f38246c803631bd7223c8dfdd509301c1
Jake Watkins <jwatkins@mozilla.com>
2020-12-01 18:17:02 -0800

    Bring vault tf into 0.12 compliance

--- terraform/vault/kms_key.tf
+++ terraform/vault/kms_key.tf
@@ -19,10 +19,10 @@ data "aws_iam_policy_document" "vault-kms-unseal" {
 
 resource "aws_iam_policy" "vault-kms-unseal" {
   name   = "Vault-KMS-Unseal"
-  policy = "${data.aws_iam_policy_document.vault-kms-unseal.json}"
+  policy = data.aws_iam_policy_document.vault-kms-unseal.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs-instance-kms-role-attachment" {
-  role       = "${aws_iam_role.ecs-task-role.name}"
-  policy_arn = "${aws_iam_policy.vault-kms-unseal.arn}"
+  role       = aws_iam_role.ecs-task-role.name
+  policy_arn = aws_iam_policy.vault-kms-unseal.arn
 }
