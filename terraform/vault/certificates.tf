60dcee75cfdc2ce79350308e9ac94478a04b1cae
Jake Watkins <jwatkins@mozilla.com>
2020-12-01 18:17:02 -0800

    Bring vault tf into 0.12 compliance

--- terraform/vault/certificates.tf
+++ terraform/vault/certificates.tf
@@ -8,14 +8,14 @@ resource "aws_acm_certificate" "vault_cert" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.vault_cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.vault_cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.relops_mozops_net.zone_id}"
-  records = ["${aws_acm_certificate.vault_cert.domain_validation_options.0.resource_record_value}"]
+  name    = aws_acm_certificate.vault_cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.vault_cert.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.relops_mozops_net.zone_id
+  records = [aws_acm_certificate.vault_cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn         = "${aws_acm_certificate.vault_cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
+  certificate_arn         = aws_acm_certificate.vault_cert.arn
+  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
 }
--- terraform/vault/ecs-task-def.tf
+++ terraform/vault/ecs-task-def.tf
@@ -1,13 +1,13 @@
 resource "aws_ecs_task_definition" "vault" {
   family                   = "vault"
-  container_definitions    = "${file("task-definitions/vault.json")}"
+  container_definitions    = file("task-definitions/vault.json")
   requires_compatibilities = ["EC2"]
   network_mode             = "awsvpc"
-  task_role_arn            = "${aws_iam_role.ecs-task-role.arn}"
+  task_role_arn            = aws_iam_role.ecs-task-role.arn
 
   tags = {
     Terraform   = "true"
-    Repo_url    = "${var.repo_url}"
+    Repo_url    = var.repo_url
     Environment = "Prod"
     Owner       = "relops@mozilla.com"
   }
