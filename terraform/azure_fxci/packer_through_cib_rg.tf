44b4eef4e031d61df1d361be43b317e3642fb0af
markcor <mcornmesser@mozilla.com>
2021-03-18 15:39:26 -0700

    Change naming convention to match per-exisiting code.

--- terraform/azure_fxci/packer_through_cib_rg.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "packer_through_cib-rg" {
-  name     = "packer_through_cib_rg"
-  location = "Central US"
-  tags = merge(local.common_tags,
-    map(
-      "Name", "packer_throuhgcib_rg"
-    )
-  )
-}
