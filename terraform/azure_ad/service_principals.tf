90da5f32a4dc68daab757fd13833ee6da168a7fe
Jake Watkins <jwatkins@mozilla.com>
2021-03-19 12:22:05 -0700

    Comment noting the map -> list reason

--- terraform/azure_ad/service_principals.tf
+++ terraform/azure_ad/service_principals.tf
@@ -1,4 +1,7 @@
 locals {
+  # The service principal resource only accept a list of strings as tags
+  # So we here we iterate over the map object, join the key and values
+  # then cast it to a list
   sp_tags = [for k, v in local.common_tags : join(":", [k, v])]
 }
 
