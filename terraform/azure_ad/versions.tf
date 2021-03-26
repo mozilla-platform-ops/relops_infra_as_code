e10acc27b685a1f5da4662333862023744de6da6
Rob Thijssen <rthijssen@gmail.com>
2021-03-26 08:13:33 +0200

    explicit tennant and subscription

--- terraform/azure_ad/versions.tf
+++ terraform/azure_ad/versions.tf
@@ -14,4 +14,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  # FXCI Azure dev/test Subscription
+  subscription_id = "108d46d5-fe9b-4850-9a7d-8c914aa6c1f0"
+  tenant_id       = "c0dc8bb0-b616-427e-8217-9513964a145b"
 }
