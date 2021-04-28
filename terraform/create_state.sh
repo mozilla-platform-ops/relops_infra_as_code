878c55d2df95eb4a9b6bf86c672b071fa4591fd3
Jake Watkins <jwatkins@mozilla.com>
2021-04-28 10:47:01 -0700

    Update create_state.sh and fix state lock file with defunc format

--- terraform/create_state.sh
+++ terraform/create_state.sh
@@ -67,7 +67,7 @@ resource "aws_dynamodb_table" "tf_state_lock_${STATE_NAME}" {
   tags = {
     Name        = "${STATE_NAME} Terraform State Lock Table"
     Terraform   = "true"
-    Repo_url    = "\${var.repo_url}"
+    Repo_url    = var.repo_url
     Environment = "prod"
     Owner       = "relops@mozilla.com"
   }
