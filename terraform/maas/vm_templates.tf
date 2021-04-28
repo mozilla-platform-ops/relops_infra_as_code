8ce296b020d71b1cc1198b276758d402ecf25f98
Jake Watkins <jwatkins@mozilla.com>
2021-04-27 18:17:53 -0700

    Remove MaaS

--- terraform/maas/vm_templates.tf
+++
@@ -1,7 +0,0 @@
-data "vsphere_virtual_machine" "mdc1_template" {
-  provider      = vsphere.mdc1
-  name          = "ubuntu-1804-packer-template-2020-05-05T16:56:52Z"
-  datacenter_id = data.vsphere_datacenter.mdc1.id
-}
-
-
