2d1dfaf01a1abf599e38df1b80b3885337b9f28a
davehouse <dhouse@mozilla.com>
2021-08-20 19:42:06 -0600

    lintsɹslʎ

--- simplemdm/m1_mdm_naming.sh
+++ simplemdm/m1_mdm_naming.sh
@@ -6,9 +6,10 @@ hn_tail=".test.releng.mslv.mozilla.com"
 for I in {002..050}; do
     hn=macmini-m1-$((10#$I))
     target_name=V-MM$I
-    serial=$(ssh -o LogLevel=ERROR -o StrictHostKeyChecking=no -o ConnectTimeout=40 -o UserKnownHostsFile=/dev/null ${hn}${hn_tail} 'ioreg -l | grep IOPlatformSerialNumber | cut -d\" -f4')
+    serial=$(ssh -o LogLevel=ERROR -o StrictHostKeyChecking=no -o ConnectTimeout=40 -o UserKnownHostsFile=/dev/null "${hn}${hn_tail}" 'ioreg -l | grep IOPlatformSerialNumber | cut -d\" -f4')
     if [[ ${#serial} -gt 0 ]]; then
-        ./nameit.sh $group "${target_name}" "${serial}" $hn
+        echo ./nameit.sh "$group" "${target_name}" "${serial}" "$hn"
+        ./nameit.sh "$group" "${target_name}" "${serial}" "$hn"
     else
         echo "$hn serial not found."
     fi
