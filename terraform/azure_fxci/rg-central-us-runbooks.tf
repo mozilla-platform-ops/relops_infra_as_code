6498ad6183858237fc884ba5942d045f2667c892
markcor <mcornmesser@mozilla.com>
2021-10-27 09:26:58 -0700

    add worker-scanner helper script and firefox ent resource group

--- terraform/azure_fxci/rg-central-us-runbooks.tf
+++ terraform/azure_fxci/rg-central-us-runbooks.tf
@@ -61,6 +61,22 @@ resource "azurerm_automation_schedule" "every-4-hours" {
   interval                = 4
   description             = "Every 4 hours"
 }
+resource "azurerm_automation_schedule" "every-2-hours" {
+    name                    = "every-2-hours"
+    resource_group_name     = azurerm_automation_account.resource-monitor.resource_group_name
+    automation_account_name = azurerm_automation_account.resource-monitor.name
+    frequency               = "Hour"
+    interval                = 2
+    description             = "Every 2 hours"
+}
+resource "azurerm_automation_schedule" "every-hour" {
+  name                    = "every-hour"
+  resource_group_name     = azurerm_automation_account.resource-monitor.resource_group_name
+  automation_account_name = azurerm_automation_account.resource-monitor.name
+  frequency               = "Hour"
+  interval                = 1
+  description             = "Every hour"
+}
 resource "azurerm_automation_schedule" "once" {
   name                    = "once"
   resource_group_name     = azurerm_automation_account.resource-monitor.resource_group_name
@@ -120,49 +136,28 @@ resource "azurerm_automation_job_schedule" "vm_day_audit" {
   runbook_name            = azurerm_automation_runbook.vm_day_audit.name
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+data "local_file" "worker_scanner_helper_ps1" {
+  filename = "runbooks/worker_scanner_helper.ps1"
+}
+resource "azurerm_automation_runbook" "worker_scanner_helper" {
+  name                    = "worker_scanner_helper"
+  location                = azurerm_automation_account.resource-monitor.location
+  resource_group_name     = azurerm_automation_account.resource-monitor.resource_group_name
+  automation_account_name = azurerm_automation_account.resource-monitor.name
+  log_verbose             = "false"
+  log_progress            = "true"
+  description             = "Delete unassociated old resources"
+  runbook_type            = "PowerShell"
+  content                 = data.local_file.worker_scanner_helper_ps1.content
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "tmp_rg_cleanup"
+    })
+  )
+}
+resource "azurerm_automation_job_schedule" "worker_scanner_helper" {
+  resource_group_name     = azurerm_automation_account.resource-monitor.resource_group_name
+  automation_account_name = azurerm_automation_account.resource-monitor.name
+  schedule_name           = azurerm_automation_schedule.every-2-hours.name
+  runbook_name            = azurerm_automation_runbook.worker_scanner_helper.name
+}
