e36f83463998d6fa98a0c0e20b09a6997fa8565a
Jake Watkins <jwatkins@mozilla.com>
2021-11-16 17:57:56 -0800

    Remove puppetagain CA codecommit

--- terraform/puppetagain_ca_codecommit/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "relops-tf-states"
-    key            = "puppetagain_ca_codecommit.tfstate"
-    dynamodb_table = "tf_state_lock_puppetagain_ca_codecommit"
-    region         = "us-west-2"
-  }
-}
-
--- terraform/puppetagain_ca_codecommit/codecommit.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_codecommit_repository" "puppetagain_ca" {
-  repository_name = "puppetagain_ca"
-  description     = "Puppetagain Root Certificate Authority Inventory"
-}
-
-output "puppetagain_ca_clone_url_ssh" {
-  value = aws_codecommit_repository.puppetagain_ca.clone_url_ssh
-}
-
