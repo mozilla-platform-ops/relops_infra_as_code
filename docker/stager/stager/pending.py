8553c0f7c6b06b8e59d1f2d320dd1aa975adf631
davehouse <5540936+davehouse@users.noreply.github.com>
2022-06-06 10:19:22 -0600

    add pending and shadow loop

---
+++ docker/stager/stager/pending.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# example:
+# ./pending.py gecko-3/b-linux
+
+import sys
+
+import taskcluster
+
+root_url = taskcluster.optionsFromEnvironment()['rootUrl']
+queue = taskcluster.Queue(taskcluster.optionsFromEnvironment())
+
+source = sys.argv[1]
+p=source.split('/')[0]
+wt=source.split('/')[1]
+source = queue.getWorkerType(p,wt)
+print(queue.pendingTasks(p,wt)['pendingTasks'])
+
---
+++ docker/stager/stager/shadow.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# expects environment vars:
+# TASKCLUSTER_CLIENT_ID
+# TASKCLUSTER_ACCESS_TOKEN
+# TASKCLUSTER_ROOT_URL
+
+
+source=releng-hardware/gecko-t-linux-talos-1804
+while true; do
+    for target in releng-hardware/gecko-t-linux-talos-g4dn-{,2,4,8,16}xlarge; do
+        pending=$(./pending.py "$target" 2>error.log ) \
+            || {
+            echo "Failed checking pending for $target"
+        }
+        if grep "AuthenticationFailed" error.log; then
+            echo -en "AuthenticationFailed\n\007"
+            sleep 30
+        fi
+        echo $target "$pending"
+        if [[ "$pending" -lt 20 ]] && [[ ${#pending} -ne 0 ]]; then
+            timeout 30s ./stage.py "$source" "$target" 30 \
+                || echo -en "Failed to stage tasks to $target\n\007"
+        fi
+    done
+    sleep 600
+done
+
