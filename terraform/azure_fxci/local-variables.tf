2e51aecea875414c81162ad7c8ac183d3782d92a
Jonathan Moss <jwmoss88+github@gmail.com>
2022-08-30 12:37:20 -0400

    add gecko-1 & gecko-3 resources to tf

---
+++ terraform/azure_fxci/local-variables.tf
@@ -0,0 +1,61 @@
+variable "gecko1" {
+    description = "storage account location"
+    type = map
+    default = {
+    "rg-central-us-gecko-1" = {
+        rgname = "central-us-gecko-1"
+        rglocation = "centralus"
+    }
+    "rg-east-us-gecko-1" = {
+        rgname = "east-us-gecko-1"
+        rglocation = "eastus"
+    }
+    "rg-east-us-2-gecko-1" = {
+        rgname = "east-us-2-gecko-1"
+        rglocation = "eastus2"
+    }
+    "rg-north-central-us-gecko-1" = {
+        rgname = "north-central-us-gecko-1"
+        rglocation = "northcentralus"
+    }
+    "rg-south-central-us-gecko-1" = {
+        rgname = "south-central-us-gecko-1"
+        rglocation = "southcentralus"
+    }
+    "rg-west-us-gecko-1" = {
+        rgname = "west-us-gecko-1"
+        rglocation = "westus"
+    }
+    }  
+}
+
+variable "gecko3" {
+    description = "storage account location"
+    type = map
+    default = {
+    "rg-central-us-gecko-3" = {
+        rgname = "central-us-gecko-3"
+        rglocation = "centralus"
+    }
+    "rg-east-us-gecko-3" = {
+        rgname = "east-us-gecko-3"
+        rglocation = "eastus"
+    }
+    "rg-east-us-2-gecko-3" = {
+        rgname = "east-us-2-gecko-3"
+        rglocation = "eastus2"
+    }
+    "rg-north-central-us-gecko-3" = {
+        rgname = "north-central-us-gecko-3"
+        rglocation = "northcentralus"
+    }
+    "rg-south-central-us-gecko-3" = {
+        rgname = "south-central-us-gecko-3"
+        rglocation = "southcentralus"
+    }
+    "rg-west-us-gecko-3" = {
+        rgname = "west-us-gecko-3"
+        rglocation = "westus"
+    }
+    }  
+}
