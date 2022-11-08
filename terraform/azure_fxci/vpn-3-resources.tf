d28785cc909293268743a8ae1609bda67939c446
Mark Cornmesser <mcornmesser@mozilla.com>
2022-11-08 11:43:16 -0800

    Add builder resources

---
+++ terraform/azure_fxci/vpn-3-resources.tf
@@ -0,0 +1,55 @@
+resource "azurerm_resource_group" "vpn3" {
+  for_each = var.vpn3
+  name     = "rg-${each.value.rgname}"
+  location = each.value.rglocation
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "${each.value.rgname}"
+    })
+  )
+}
+
+resource "azurerm_storage_account" "vpn3" {
+  for_each                 = var.vpn3
+  name                     = replace("sa${each.value.rgname}", "/\\W|_|\\s/", "")
+  resource_group_name      = azurerm_resource_group.vpn3[each.key].name
+  location                 = each.value.rglocation
+  account_replication_type = "GRS"
+  account_tier             = "Standard"
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "${each.value.rgname}"
+    })
+  )
+}
+
+resource "azurerm_network_security_group" "vpn3" {
+  for_each            = var.vpn3
+  name                = "nsg-${each.value.rgname}"
+  resource_group_name = azurerm_resource_group.vpn3[each.key].name
+  location            = each.value.rglocation
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "${each.value.rgname}"
+    })
+  )
+}
+
+resource "azurerm_virtual_network" "vpn3" {
+  for_each            = var.vpn3
+  name                = "vn-${each.value.rgname}"
+  resource_group_name = azurerm_resource_group.vpn3[each.key].name
+  location            = each.value.rglocation
+  address_space       = ["10.0.0.0/24"]
+  dns_servers         = ["1.1.1.1", "1.1.1.0"]
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "${each.value.rgname}"
+    })
+  )
+  subnet {
+    name           = "sn-${each.value.rgname}"
+    address_prefix = "10.0.0.0/24"
+    security_group = azurerm_network_security_group.vpn3[each.key].id
+  }
+}
