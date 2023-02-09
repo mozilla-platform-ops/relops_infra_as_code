132e0e007b5149dbd1f7dd651ada009aca04ccb2
Mark Cornmesser <mcornmesser@mozilla.com>
2023-02-09 12:15:37 -0800

    remove exited IAM users

--- terraform/base/iam_users.tf
+++ terraform/base/iam_users.tf
@@ -10,15 +10,8 @@ resource "aws_iam_user" "jwatkins" {
 resource "aws_iam_user" "aerickson" {
   name = "aerickson"
 }
-resource "aws_iam_user" "dhouse" {
-  name = "dhouse"
-}
 resource "aws_iam_user" "mcornmesser" {
   name = "mcornmesser"
-}
-
-resource "aws_iam_user" "ajvb" {
-  name = "ajvb"
 }
 
 resource "aws_iam_user" "jmoss" {
