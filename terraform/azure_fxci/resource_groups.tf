15978c8086f0cebec9c19c25468fde5e112e1b98
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-03-01 08:31:19 -0500

    Update resource_groups.tf

--- terraform/azure_fxci/resource_groups.tf
+++ terraform/azure_fxci/resource_groups.tf
@@ -8,3 +8,14 @@ resource "azurerm_resource_group" "relops_terraform" {
     })
   )
 }
+
+resource "azurerm_resource_group" "windows7" {
+  name     = "rg-north-central-us-windows7"
+  location = "North Central US"
+
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "relops_terraform"
+    })
+  )
+}
