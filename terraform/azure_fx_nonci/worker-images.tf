db06379520e42f3e720bcf7f26cfea8595b56ea3
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2023-05-10 15:36:23 -0400

    Remove -alpha image definitions

--- terraform/azure_fx_nonci/worker-images.tf
+++ terraform/azure_fx_nonci/worker-images.tf
@@ -36,38 +36,8 @@ resource "azurerm_shared_image" "win11_64_2009" {
   }
 }
 
-resource "azurerm_shared_image" "win11_64_2009_alpha" {
-  name                = "win11-64-2009-alpha"
-  gallery_name        = azurerm_shared_image_gallery.packer.name
-  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
-  location            = azurerm_resource_group.rg-packer-worker-images.location
-  os_type             = "Windows"
-  hyper_v_generation  = "V2"
-
-  identifier {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-11"
-    sku       = "win11-22h2-avd"
-  }
-}
-
 resource "azurerm_shared_image" "win10_64_2009" {
   name                = "win10-64-2009"
-  gallery_name        = azurerm_shared_image_gallery.packer.name
-  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
-  location            = azurerm_resource_group.rg-packer-worker-images.location
-  os_type             = "Windows"
-  hyper_v_generation  = "V2"
-
-  identifier {
-    publisher = "MicrosoftWindowsDesktop"
-    offer     = "Windows-10"
-    sku       = "win10-22h2-avd"
-  }
-}
-
-resource "azurerm_shared_image" "win10_64_2009_alpha" {
-  name                = "win10-64-2009-alpha"
   gallery_name        = azurerm_shared_image_gallery.packer.name
   resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
   location            = azurerm_resource_group.rg-packer-worker-images.location
