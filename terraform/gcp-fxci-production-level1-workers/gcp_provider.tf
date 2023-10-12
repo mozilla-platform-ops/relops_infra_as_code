c5016c3ba929bf25d2c0a1f99517adf4032ab298
Andrew Erickson <aerickson@mozilla.com>
2023-10-12 15:46:33 -0700

    add gcp provider

---
+++ terraform/gcp-fxci-production-level1-workers/gcp_provider.tf
@@ -0,0 +1,7 @@
+provider "google" {
+  project = "fxci-production-level1-workers"
+  # not sure what these should be
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
---
+++ terraform/gcp-fxci-production-level1-workers/terraform.tfvars
@@ -0,0 +1,5 @@
+tag_project_name = "gcp-fxci-production-level1-workers"
+
+tag_production_state = "production"
+
+tag_owner_email = "aerickson@mozilla.com"
