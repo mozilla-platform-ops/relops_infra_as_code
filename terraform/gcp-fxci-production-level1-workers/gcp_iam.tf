5239de6ca92ad8bdb4709d94b6f3cd1552886999
Andrew Erickson <aerickson@mozilla.com>
2023-10-12 16:27:29 -0700

    wip 2

--- terraform/gcp-fxci-production-level1-workers/gcp_iam.tf
+++ terraform/gcp-fxci-production-level1-workers/gcp_iam.tf
@@ -8,18 +8,47 @@ variable "translations_users" {
     description = "list of usernames"
 }
 
-resource "google_project_iam_member" "releng-users" {
+# TODO: use this?
+# roles/viewer has a ton of permissions, reduce
+#   - https://console.cloud.google.com/iam-admin/roles/details/roles%3Cviewer?project=fxci-production-level1-workers
+# resource "google_organization_iam_custom_role" "relsre-compute-viewer" {
+#   role_id     = "relsre-compute-viewer"
+#   org_id      = "887720501152"  # fxci-production-level1-workers
+#   title       = "ReleaseSRE 'compute viewer' custom role"
+#   description = "a read-only role that allows viewing of compute resources"
+#   permissions = ["compute.instances.list", "compute.instances.get"]
+# }
+
+# roles/monitoring.viewer
+resource "google_project_iam_member" "releng-users-monitoring" {
     for_each = "${var.releng_users}"
 
   project = "fxci-production-level1-workers"
-  role    = "compute.instances.list"
+  role    = "roles/monitoring.viewer"
   member  = "user:${each.value}@mozilla.com"
 }
 
-resource "google_project_iam_member" "translations-users" {
+resource "google_project_iam_member" "translations-users-monitoring" {
     for_each = "${var.releng_users}"
 
   project = "fxci-production-level1-workers"
-  role    = "compute.instances.list"
+  role    = "roles/monitoring.viewer"
+  member  = "user:${each.value}@mozilla.com"
+}
+
+# roles/compute.viewer
+resource "google_project_iam_member" "releng-users-compute" {
+    for_each = "${var.releng_users}"
+
+  project = "fxci-production-level1-workers"
+  role    = "roles/compute.viewer"
+  member  = "user:${each.value}@mozilla.com"
+}
+
+resource "google_project_iam_member" "translations-users-compute" {
+    for_each = "${var.releng_users}"
+
+  project = "fxci-production-level1-workers"
+  role    = "roles/compute.viewer"
   member  = "user:${each.value}@mozilla.com"
 }
