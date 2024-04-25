2522155f70fc9d067851a20bc6d4d3db1879bd71
Andrew Erickson <aerickson@mozilla.com>
2024-04-25 11:13:03 -0700

    fxci-l1: grant bhearsum simulation perms (#160)

--- terraform/gcp-fxci-production-level1-workers/gcp_iam.tf
+++ terraform/gcp-fxci-production-level1-workers/gcp_iam.tf
@@ -87,3 +87,20 @@ resource "google_project_iam_member" "translations-users-monitoring-editor" {
   role    = "roles/monitoring.editor"
   member  = "user:${each.value}@mozilla.com"
 }
+
+# bhearsum wants compute.instances.simulateMaintenanceEvent for spot instance termination testing
+
+# the two pre-existing roles with this perm have too much, create a custom role
+resource "google_project_iam_custom_role" "my-custom-role" {
+  role_id     = "relops_compute_simulate_maintenance"
+  title       = "Relops Compute Simulate Maintenance Role"
+  description = "A role that allows compute.instances.simulateMaintenanceEvent"
+  permissions = ["compute.instances.simulateMaintenanceEvent"]
+}
+
+resource "google_project_iam_member" "bhearsum-compute-simulate-maintenance" {
+  project = "fxci-production-level1-workers"
+  # role    = "roles/compute.instances.simulateMaintenanceEvent"
+  role = "projects/fxci-production-level1-workers/roles/relops_compute_simulate_maintenance"
+  member  = "user:bhearsum@mozilla.com"
+}
