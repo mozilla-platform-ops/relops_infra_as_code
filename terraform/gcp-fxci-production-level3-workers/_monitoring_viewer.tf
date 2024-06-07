d2550b200587aeeb3504eb58b36dba542a0b73bc
Andrew Erickson <aerickson@mozilla.com>
2024-06-07 16:13:12 -0500

    l3: add monitoring viewer role to svc account

---
+++ terraform/gcp-fxci-production-level3-workers/_monitoring_viewer.tf
@@ -0,0 +1,11 @@
+# requested by ahal in https://mozilla-hub.atlassian.net/browse/RELOPS-984
+
+# So could someone please grant the Monitoring Viewer role to the
+# fxci-etl@moz-fx-dev-releng.iam.gserviceaccount.com service account?
+# In both the fxci-production-level1-workers and fxci-production-level3-workers projects.
+
+resource "google_project_iam_member" "monitoring-viewer-svc-account" {
+  project = "fxci-production-level1-workers"
+  role = "roles/monitoring.viewer"
+  member  = "serviceAccount:fxci-etl@moz-fx-dev-releng.iam.gserviceaccount.com"
+}
