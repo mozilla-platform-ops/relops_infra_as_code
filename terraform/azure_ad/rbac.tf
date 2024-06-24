21f96c78fab706c28377ef3e4fb1551955dee4c3
Jonathan Moss <jwmoss88@gmail.com>
2024-06-24 17:20:18 -0400

    assign splunk event hub to subscriptions

--- terraform/azure_ad/rbac.tf
+++ terraform/azure_ad/rbac.tf
@@ -10,6 +10,13 @@ resource "azurerm_role_assignment" "infrasec_reader" {
   principal_id         = data.azuread_group.infrasec.object_id
 }
 
+resource "azurerm_role_assignment" "splunkeventhub" {
+  for_each             = toset(var.azure_subscriptions)
+  scope                = each.value
+  role_definition_name = "Azure Event Hubs Data Receiver"
+  principal_id         = azuread_service_principal.splunkeventhub.object_id
+}
+
 variable "azure_subscriptions" {
   type = list(any)
   description = "List of subscriptions to be assigned to infrastructure team"
