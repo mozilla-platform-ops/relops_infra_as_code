167002cc5f3c56bd16bbb85efcda4235a4b2b007
Andrew Erickson <aerickson@mozilla.com>
2024-07-12 13:20:49 -0700

    add l3 r/o access

--- terraform/gcp-fxci-production-level3-workers/_vars.tf
+++ terraform/gcp-fxci-production-level3-workers/_vars.tf
@@ -2,3 +2,13 @@ variable "releng_users" {
     type = set(string)
     description = "list of usernames"
 }
+
+variable "translations_users" {
+    type = set(string)
+    description = "list of usernames"
+}
+
+variable "read_only_users" {
+    type = set(string)
+    description = "list of usernames"
+}
