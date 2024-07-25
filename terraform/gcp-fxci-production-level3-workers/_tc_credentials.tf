5d66427552a177b47dfb8c51a491c50527137ac0
Andrew Erickson <aerickson@mozilla.com>
2024-07-24 21:27:20 -0700

    add todos

--- terraform/gcp-fxci-production-level3-workers/_tc_credentials.tf
+++ terraform/gcp-fxci-production-level3-workers/_tc_credentials.tf
@@ -4,3 +4,7 @@ resource "google_project_iam_member" "worker-manager-compute-admin" {
   role    = "roles/compute.admin"
   member  = "serviceAccount:taskcluster-worker-manager@fxci-production-level3-workers.iam.gserviceaccount.com"
 }
+
+# TODO:
+# - taskcluster-worker@fxci-production-level1-workers.iam.gserviceaccount.com
+# - deploy-prod@fxci-production-level1-workers.iam.gserviceaccount.com
