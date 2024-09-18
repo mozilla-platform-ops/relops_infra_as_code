90367f54eec46445021926e782a2c60b57c71ce4
Andrew Erickson <aerickson@mozilla.com>
2024-09-18 16:54:49 -0400

    add taskcluster imaging top level tf project

---
+++ terraform/gcp-taskcluster-imaging/_gcp_provider.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  # name: taskcluster-prod
+  # gcp id: moz-fx-taskcluster-prod-4b87
+  project = "taskcluster-prod"
+  # not sure what these should be
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
---
+++ terraform/gcp-taskcluster-imaging/terraform.tfvars
@@ -0,0 +1,5 @@
+tag_project_name = "gcp-taskcluster-imaging"
+
+tag_production_state = "production"
+
+tag_owner_email = "aerickson@mozilla.com"
