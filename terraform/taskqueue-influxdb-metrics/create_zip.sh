a418c36298f405c074d2eaa7facc6291df452cd0
Andrew Erickson <aerickson@mozilla.com>
2024-10-08 00:33:45 -0500

    comments, add missing dep to pipfile

--- terraform/taskqueue-influxdb-metrics/create_zip.sh
+++ terraform/taskqueue-influxdb-metrics/create_zip.sh
@@ -14,13 +14,11 @@ cd package || exit 1
 touch setup.cfg
 printf '[install]\nprefix=  \n' > setup.cfg
 
-# install deps to . with pip3
-# pip3 install influxdb --target .
-# equivalent pipenv command:
-#   https://github.com/pypa/pipenv/issues/746
+# install deps to .
 pipenv requirements > requirements.txt
 pipenv run pip install -r requirements.txt --target .
-#pipenv requirements > requirements.txt
+
+# create the zip
 zip -r9 ../function.zip .
 cd ..
 zip -g function.zip function.py
