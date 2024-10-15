04f1f873db8382e39de2408344109c8068366e3a
Mark Cornmesser <mcornmesser@mozilla.com>
2024-10-15 10:11:06 -0700

    update gallery name for a64 24h2 iamges

--- terraform/azure_fxci/worker-images.tf
+++ terraform/azure_fxci/worker-images.tf
@@ -129,7 +129,7 @@ resource "azurerm_shared_image" "win11_64_2009" {
 }
 ## win11-a64-24h2-builder
 resource "azurerm_shared_image_gallery" "win11_a64_24h2_builder_alpha" {
-  name                = "ronin_b_windows11_a64_24h2_builder_alpha"
+  name                = "win11_a64_24h2_builder_alpha"
   resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
   location            = azurerm_resource_group.rg-packer-worker-images.location
   description         = "win11-a64-24h2-builder-alpha"
@@ -160,7 +160,7 @@ resource "azurerm_shared_image" "win11_a64_24h2_builder_alpha" {
 
 ## win11-a64-24h2-tester
 resource "azurerm_shared_image_gallery" "win11_a64_24h2_tester_alpha" {
-  name                = "ronin_b_windows11_a64_24h2_tester_alpha"
+  name                = "win11_a64_24h2_tester_alpha"
   resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
   location            = azurerm_resource_group.rg-packer-worker-images.location
   description         = "win11-a64-24h2-tester-alpha"
@@ -175,68 +175,6 @@ resource "azurerm_shared_image_gallery" "win11_a64_24h2_tester_alpha" {
 resource "azurerm_shared_image" "win11_a64_24h2_tester_alpha" {
   name                = "win11_a64_24h2_tester_alpha"
   gallery_name        = azurerm_shared_image_gallery.win11_a64_24h2_tester_alpha.name
-  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
-  location            = azurerm_resource_group.rg-packer-worker-images.location
-  os_type             = "Windows"
-  release_note_uri    = "https://github.com/mozilla-platform-ops/worker-images/releases"
-  hyper_v_generation  = "V2"
-  architecture        = "Arm64"
-
-  identifier {
-    publisher = "windows11preview-arm64"
-    offer     = "Windows-11"
-    sku       = "win11-24h2-ent"
-  }
-}
-
-## win11-a64-23h2-builder
-resource "azurerm_shared_image_gallery" "win11_a64_23h2_builder_alpha" {
-  name                = "ronin_b_windows11_a64_23h2_builder_alpha"
-  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
-  location            = azurerm_resource_group.rg-packer-worker-images.location
-  description         = "win11-a64-23h2-builder-alpha"
-
-  tags = merge(local.common_tags,
-    tomap({
-      "Name" = "rg-packer-worker-images"
-    })
-  )
-}
-
-resource "azurerm_shared_image" "win11_a64_23h2_builder_alpha" {
-  name                = "win11_a64_23h2_builder_alpha"
-  gallery_name        = azurerm_shared_image_gallery.win11_a64_23h2_builder_alpha.name
-  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
-  location            = azurerm_resource_group.rg-packer-worker-images.location
-  os_type             = "Windows"
-  release_note_uri    = "https://github.com/mozilla-platform-ops/worker-images/releases"
-  hyper_v_generation  = "V2"
-  architecture        = "Arm64"
-
-  identifier {
-    publisher = "windows11preview-arm64"
-    offer     = "Windows-11"
-    sku       = "win11-23h2-ent"
-  }
-}
-
-## win11-a64-23h2-tester
-resource "azurerm_shared_image_gallery" "win11_a64_23h2_tester_alpha" {
-  name                = "ronin_b_windows11_a64_23h2_tester_alpha"
-  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
-  location            = azurerm_resource_group.rg-packer-worker-images.location
-  description         = "win11-a64-23h2-tester-alpha"
-
-  tags = merge(local.common_tags,
-    tomap({
-      "Name" = "rg-packer-worker-images"
-    })
-  )
-}
-
-resource "azurerm_shared_image" "win11_a64_23h2_tester_alpha" {
-  name                = "win11_a64_23h2_tester_alpha"
-  gallery_name        = azurerm_shared_image_gallery.win11_a64_23h2_tester_alpha.name
   resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
   location            = azurerm_resource_group.rg-packer-worker-images.location
   os_type             = "Windows"
