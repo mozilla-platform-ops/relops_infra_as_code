32c6e8c074598cea39bdbf329141d562b2da5e94
Mark Cornmesser <mcornmesser@mozilla.com>
2024-10-17 10:31:44 -0700

    add prod win 11 a64 galleries

--- terraform/azure_fxci/worker-images.tf
+++ terraform/azure_fxci/worker-images.tf
@@ -158,6 +158,37 @@ resource "azurerm_shared_image" "win11_a64_24h2_builder_alpha" {
   }
 }
 
+resource "azurerm_shared_image_gallery" "win11_a64_24h2_builder" {
+  name                = "win11_a64_24h2_builder"
+  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
+  location            = azurerm_resource_group.rg-packer-worker-images.location
+  description         = "win11-a64-24h2-builder"
+
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "rg-packer-worker-images"
+    })
+  )
+}
+
+resource "azurerm_shared_image" "win11_a64_24h2_builder" {
+  name                = "win11_a64_24h2_builder"
+  gallery_name        = azurerm_shared_image_gallery.win11_a64_24h2_builder.name
+  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
+  location            = azurerm_resource_group.rg-packer-worker-images.location
+  os_type             = "Windows"
+  release_note_uri    = "https://github.com/mozilla-platform-ops/worker-images/releases"
+  hyper_v_generation  = "V2"
+  architecture        = "Arm64"
+
+  identifier {
+    publisher = "windows11preview-arm64"
+    offer     = "Windows-11"
+    sku       = "win11-24h2-ent"
+  }
+}
+
+
 ## win11-a64-24h2-tester
 resource "azurerm_shared_image_gallery" "win11_a64_24h2_tester_alpha" {
   name                = "win11_a64_24h2_tester_alpha"
@@ -174,6 +205,36 @@ resource "azurerm_shared_image_gallery" "win11_a64_24h2_tester_alpha" {
 
 resource "azurerm_shared_image" "win11_a64_24h2_tester_alpha" {
   name                = "win11_a64_24h2_tester_alpha"
+  gallery_name        = azurerm_shared_image_gallery.win11_a64_24h2_tester.name
+  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
+  location            = azurerm_resource_group.rg-packer-worker-images.location
+  os_type             = "Windows"
+  release_note_uri    = "https://github.com/mozilla-platform-ops/worker-images/releases"
+  hyper_v_generation  = "V2"
+  architecture        = "Arm64"
+
+  identifier {
+    publisher = "windows11preview-arm64"
+    offer     = "Windows-11"
+    sku       = "win11-24h2-ent"
+  }
+}
+
+resource "azurerm_shared_image_gallery" "win11_a64_24h2_tester" {
+  name                = "win11_a64_24h2_tester"
+  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
+  location            = azurerm_resource_group.rg-packer-worker-images.location
+  description         = "win11-a64-24h2-tester"
+
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "rg-packer-worker-images"
+    })
+  )
+}
+
+resource "azurerm_shared_image" "win11_a64_24h2_tester" {
+  name                = "win11_a64_24h2_tester"
   gallery_name        = azurerm_shared_image_gallery.win11_a64_24h2_tester_alpha.name
   resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
   location            = azurerm_resource_group.rg-packer-worker-images.location
