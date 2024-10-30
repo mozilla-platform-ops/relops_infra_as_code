7f584dce082f46017c3f8e1007eea08c4595fe3d
Jonathan Moss <jwmoss88@gmail.com>
2024-10-30 14:55:01 -0400

    Switch to blockblobstorage and create hgbundle container

--- terraform/azure_fxci/hg-bundle.tf
+++ terraform/azure_fxci/hg-bundle.tf
@@ -1,3 +1,8 @@
+# Adding
+# A single blob supports up to 500 requests per second.
+# If you have multiple clients that need to read the same blob and you might exceed this limit, then consider using a block blob storage account.
+# A block blob storage account provides a higher request rate, or I/O operations per second (IOPS).
+
 locals {
   tags = {
     terraform        = "true"
@@ -37,6 +42,14 @@ resource "azurerm_storage_account" "hgbundle" {
   resource_group_name      = azurerm_resource_group.hgbundle[each.value].name
   account_replication_type = "LRS"
   location                 = each.value
-  account_tier             = "Standard"
+  account_tier             = "Premium"
   tags                     = local.tags
+  account_kind             = "BlockBlobStorage"
+}
+
+resource "azurerm_storage_container" "hgbundle" {
+  for_each              = local.regions
+  name                  = "hgbundle"
+  storage_account_name  = azurerm_storage_account.hgbundle[each.value].name
+  container_access_type = "container"
 }
