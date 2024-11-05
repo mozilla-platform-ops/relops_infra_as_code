1e143f969576c91c1d0f70d99de4e122b54db8c8
Jonathan Moss <jwmoss88@gmail.com>
2024-11-05 09:43:48 -0500

    Add win11-64-24h2 shared image

--- terraform/azure_fxci/worker-images.tf
+++ terraform/azure_fxci/worker-images.tf
@@ -249,3 +249,32 @@ resource "azurerm_shared_image" "win11_a64_24h2_tester" {
     sku       = "win11-24h2-ent"
   }
 }
+
+resource "azurerm_shared_image_gallery" "win11_64_24h2" {
+  name                = "win11_64_24h2"
+  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
+  location            = azurerm_resource_group.rg-packer-worker-images.location
+  description         = "win11-64-24h2"
+
+  tags = merge(local.common_tags,
+    tomap({
+      "Name" = "rg-packer-worker-images"
+    })
+  )
+}
+
+resource "azurerm_shared_image" "win11_64_24h2" {
+  name                = "win11_64_24h2"
+  gallery_name        = azurerm_shared_image_gallery.win11_64_24h2.name
+  resource_group_name = azurerm_resource_group.rg-packer-worker-images.name
+  location            = azurerm_resource_group.rg-packer-worker-images.location
+  os_type             = "Windows"
+  release_note_uri    = "https://github.com/mozilla-platform-ops/worker-images/releases"
+  hyper_v_generation  = "V2"
+
+  identifier {
+    publisher = "MicrosoftWindowsDesktop"
+    offer     = "Windows-11"
+    sku       = "win11-24h2-avd"
+  }
+}
