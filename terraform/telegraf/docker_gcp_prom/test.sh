8cefdf7deeb8ac7f7a2e8ec9a08922bb98b8037b
Andrew Erickson <aerickson@mozilla.com>
2024-11-04 21:07:11 -0800

    imagining issues with latest?

--- terraform/telegraf/docker_gcp_prom/test.sh
+++ terraform/telegraf/docker_gcp_prom/test.sh
@@ -6,7 +6,8 @@ set -e
 # run `TELEGRAF_CONFIG='telegraf-aerickson-testing.conf' ./docker_run` in another window first
 
 curl -q http://localhost:9273/metrics 2>/dev/null \
-  | grep 'fc_ceph_df_usage_percent{crush_type="osd",host="10c073ffaf06"} 35.85032'
+  | grep 'fc_ceph_df_usage_percent'
+  # | grep 'fc_ceph_df_usage_percent{crush_type="osd",host="10c073ffaf06"} 35.85032'
 
 
 echo ""
