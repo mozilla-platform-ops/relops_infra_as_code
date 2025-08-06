6d2efe44c9745920c4c4f518fc5f6823d1c621df
Jonathan Moss <jwmoss88@gmail.com>
2025-08-06 14:16:55 -0400

    delete vpn-3 azure nat gw resources

--- terraform/azure_trusted_fxci/vpn-3/main.tf
+++ terraform/azure_trusted_fxci/vpn-3/main.tf
@@ -22,37 +22,11 @@ locals {
   provisioner = "vpn-3"
 }
 
-resource "azurerm_resource_group" "this" {
-  for_each = local.location_map
-  name     = "rg2-${each.value}-${local.provisioner}"
-  location = each.key
-}
-
 resource "azurerm_resource_group" "nongw" {
   for_each = local.location_map
   name     = "rg-${each.value}-${local.provisioner}"
   location = each.key
   tags     = local.tags
-}
-
-module "vpn-3" {
-  for_each                            = local.location_map
-  source                              = "../../azure_modules/workerPool"
-  location                            = each.key
-  azurerm_virtual_network_name        = "vn2-${each.value}-${local.provisioner}"
-  azurerm_subnet_name                 = "sn2-${each.value}-${local.provisioner}"
-  resource_group_name                 = azurerm_resource_group.this[each.key].name
-  azurerm_network_security_group_name = "nsg2-${each.value}-${local.provisioner}"
-  azurerm_public_ip_prefix_name       = "ippre2-${each.value}-${local.provisioner}"
-  nat_gateway_name                    = "ng2-${each.value}-${local.provisioner}"
-  azurerm_storage_account_name        = "sa2${each.value}-${local.provisioner}"
-  vnet_address_space                  = local.config.defaults.vnet_address_space
-  vnet_dns_servers                    = local.config.defaults.vnet_dns_servers
-  subnet_address_prefixes             = local.config.defaults.subnet_address_prefixes
-  provisioner                         = local.provisioner
-  tags                                = local.tags
-  nsg_security_rules                  = []
-  depends_on                          = [azurerm_resource_group.this]
 }
 
 module "vpn-3_nogw" {
