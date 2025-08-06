704adaffa4be2bb239d221de903333696523dc74
Jonathan Moss <jwmoss88@gmail.com>
2025-08-06 13:28:57 -0400

    remove mozilla-1 azure nat gw resources

--- terraform/azure_fxci/mozilla-1/main.tf
+++ terraform/azure_fxci/mozilla-1/main.tf
@@ -23,37 +23,12 @@ locals {
   provisioner_short = "moz1"
 }
 
-resource "azurerm_resource_group" "this" {
-  for_each = local.location_map
-  name     = "rg2-${each.value}-${local.provisioner}"
-  location = each.key
-}
 
 resource "azurerm_resource_group" "nongw" {
   for_each = local.location_map
   name     = "rg-${each.value}-${local.provisioner}"
   location = each.key
   tags     = local.tags
-}
-
-module "mozilla-1" {
-  for_each                            = local.location_map
-  source                              = "../../azure_modules/workerPool"
-  location                            = each.key
-  azurerm_virtual_network_name        = "vn2-${each.value}-${local.provisioner}"
-  azurerm_subnet_name                 = "sn2-${each.value}-${local.provisioner}"
-  resource_group_name                 = azurerm_resource_group.this[each.key].name
-  azurerm_network_security_group_name = "nsg2-${each.value}-${local.provisioner}"
-  azurerm_public_ip_prefix_name       = "ippre2-${each.value}-${local.provisioner}"
-  nat_gateway_name                    = "ng2-${each.value}-${local.provisioner}"
-  azurerm_storage_account_name        = "sa2${each.value}-${local.provisioner_short}"
-  vnet_address_space                  = local.config.defaults.vnet_address_space
-  vnet_dns_servers                    = local.config.defaults.vnet_dns_servers
-  subnet_address_prefixes             = local.config.defaults.subnet_address_prefixes
-  provisioner                         = local.provisioner
-  tags                                = local.tags
-  nsg_security_rules                  = []
-  depends_on                          = [azurerm_resource_group.this]
 }
 
 module "mozilla-1_nogw" {
