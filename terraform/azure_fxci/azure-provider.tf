6c7b8dd5c8a75c9865bbff71d67d14a084b9f0b5
Jonathan Moss <jwmoss88@gmail.com>
2025-09-03 16:38:11 -0400

    fix cost export for azure infrasec

--- terraform/azure_fxci/azure-provider.tf
+++ terraform/azure_fxci/azure-provider.tf
@@ -6,3 +6,9 @@ provider "azurerm" {
   subscription_id = "108d46d5-fe9b-4850-9a7d-8c914aa6c1f0"
   tenant_id       = "c0dc8bb0-b616-427e-8217-9513964a145b"
 }
+
+# Configure the AzAPI Provider
+provider "azapi" {
+  subscription_id = "108d46d5-fe9b-4850-9a7d-8c914aa6c1f0"
+  tenant_id       = "c0dc8bb0-b616-427e-8217-9513964a145b"
+}
