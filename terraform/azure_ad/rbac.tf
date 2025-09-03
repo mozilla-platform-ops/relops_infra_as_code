9ce827aa4813c08a385fb0ded79e4ff85f6c47a8
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-09-03 17:24:47 -0400

    add releng to billing reader and fix rbac role for releng (#248)
    * add releng to billing reader
    
    * fix billing reader and add releng as reader to subscriptions
    
    * restore contributor access to non-ci devtest sub
    
    * fix formatting issues
    
    * add releng as reader to other subs and contributor to fx non-ci sub

--- terraform/azure_ad/rbac.tf
+++ terraform/azure_ad/rbac.tf
@@ -3,6 +3,42 @@ data "azuread_group" "infrasec" {
   security_enabled = true
 }
 
+data "azuread_group" "releng" {
+  display_name     = "Releng"
+  security_enabled = true
+}
+
+# Assign Billing Reader role to the specified group across all subscriptions
+resource "azurerm_role_assignment" "billing_reader_releng" {
+  for_each             = toset(var.azure_subscriptions)
+  scope                = each.value
+  role_definition_name = "Billing Reader"
+  principal_id         = data.azuread_group.releng.object_id
+}
+
+## contributor to non-ci sub
+resource "azurerm_role_assignment" "releng_contributor" {
+  for_each = toset([
+    "/subscriptions/0a420ff9-bc77-4475-befc-a05071fc92ec",
+  ])
+  scope                = each.value
+  role_definition_name = "Contributor"
+  principal_id         = data.azuread_group.releng.object_id
+}
+
+## reader to the others
+resource "azurerm_role_assignment" "releng_reader" {
+  for_each = toset([
+    "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0",
+    "/subscriptions/8a205152-b25a-417f-a676-80465535a6c9",
+    "/subscriptions/a30e97ab-734a-4f3b-a0e4-c51c0bff0701",
+    "/subscriptions/9b9774fb-67f1-45b7-830f-aafe07a94396"
+  ])
+  scope                = each.value
+  role_definition_name = "reader"
+  principal_id         = data.azuread_group.releng.object_id
+}
+
 resource "azurerm_role_assignment" "infrasec_reader" {
   for_each             = toset(var.azure_subscriptions)
   scope                = each.value
@@ -18,7 +54,7 @@ resource "azurerm_role_assignment" "splunkeventhub" {
 }
 
 variable "azure_subscriptions" {
-  type = list(any)
+  type        = list(any)
   description = "List of subscriptions to be assigned to infrastructure team"
-  default = []
+  default     = []
 }
