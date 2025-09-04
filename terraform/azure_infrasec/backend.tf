4d7a43d9bc160739b3fd38993f54d0eb904d4d76
Jonathan Moss <jwmoss88@gmail.com>
2025-09-04 17:20:00 -0400

    remove all files from azure infrasec and start over

--- terraform/azure_infrasec/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "relops-tf-states"
-    key            = "azure_infrasec.tfstate"
-    use_lockfile   = true
-    region         = "us-west-2"
-  }
-}
--- terraform/azure_infrasec/main.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  infra_sec_subscription = "9b9774fb-67f1-45b7-830f-aafe07a94396"
-  mozilla_tenant_id      = "c0dc8bb0-b616-427e-8217-9513964a145b"
-}
