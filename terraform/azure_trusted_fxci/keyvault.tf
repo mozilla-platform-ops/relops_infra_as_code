2ada88b2b35d7f699c58956dbfa8cc61a5479d27
Jonathan Moss <jwmoss88@gmail.com>
2025-09-24 16:55:48 -0400

    formatting

--- terraform/azure_trusted_fxci/keyvault.tf
+++ terraform/azure_trusted_fxci/keyvault.tf
@@ -3,10 +3,10 @@ locals {
     terraform       = "true"
     source_repo_url = "https://github.com/mozilla-platform-ops/relops_infra_as_code"
   }
-  tenant_id     = "c0dc8bb0-b616-427e-8217-9513964a145b"
-  location      = "Central US"
-  locationshort = "central-us"
-  relops_object_id = "cb79b99f-fdaa-4e0d-a2c8-c5841890fa74"
+  tenant_id               = "c0dc8bb0-b616-427e-8217-9513964a145b"
+  location                = "Central US"
+  locationshort           = "central-us"
+  relops_object_id        = "cb79b99f-fdaa-4e0d-a2c8-c5841890fa74"
   worker_images_object_id = "545b9db8-f89c-4380-becf-79316c396a16"
 }
 
@@ -17,11 +17,11 @@ resource "azurerm_resource_group" "cot" {
 }
 
 resource "azurerm_key_vault" "cot" {
-  name                          = "kv-${local.locationshort}-cot"
-  location                      = azurerm_resource_group.cot.location
-  resource_group_name           = azurerm_resource_group.cot.name
-  tenant_id                     = local.tenant_id
-  sku_name                      = "standard"
+  name                = "kv-${local.locationshort}-cot"
+  location            = azurerm_resource_group.cot.location
+  resource_group_name = azurerm_resource_group.cot.name
+  tenant_id           = local.tenant_id
+  sku_name            = "standard"
 
   # Access policy for the current user/service principal running Terraform
   access_policy {
