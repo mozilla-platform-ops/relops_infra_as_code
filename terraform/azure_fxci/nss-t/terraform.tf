9053c85f4706013f47299daf036c0638711906da
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-09-26 14:03:38 -0400

    add missing regions and worker pools (#256)

---
+++ terraform/azure_fxci/nss-t/terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket         = "relops-tf-states"
+    key            = "azure_fxci_nss_t.tfstate"
+    use_lockfile   = true
+    region         = "us-west-2"
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["961225894672"]
+  alias               = "us-west-2"
+  region              = "us-west-2"
+}
+
+# Configure the Azure Provider
+provider "azurerm" {
+  features {}
+
+  # FXCI Azure dev/test Subscription
+  subscription_id = "108d46d5-fe9b-4850-9a7d-8c914aa6c1f0"
+  tenant_id       = "c0dc8bb0-b616-427e-8217-9513964a145b"
+}
