fbbbb4e5ec2d42345a796c07457eb65654e91205
Mark Cornmesser <mcornmesser@mozilla.com>
2025-10-06 06:46:41 -0700

    corrections

--- terraform/azure_0DIN/finops.tf
+++ terraform/azure_0DIN/finops.tf
@@ -1,31 +1,7 @@
-resource "azurerm_resource_group" "this" {
-  name     = "rg-azure-cost-mgmt"
-  location = "East US"
-  tags = merge(local.common_tags,
-    tomap({
-      "Name" = "rg-azure-cost-mgmt"
-    })
-  )
-}
-
-# storage account names can only consist of lowercase letters and numbers
-resource "azurerm_storage_account" "this" {
-  name                     = "safinopsdata"
-  resource_group_name      = azurerm_resource_group.this.name
-  location                 = "East us"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-  tags = merge(local.common_tags,
-    tomap({
-      "Name" = "safinopsdata"
-    })
-  )
-}
-
-## Azure Cost Exports for Azure Infrastructure Security Subscription
+## Azure Cost Exports
 resource "azapi_resource" "azure_zero_din_cost_export_actual" {
   type      = "Microsoft.CostManagement/exports@2025-03-01"
-  name      = "azure-infrasec_actual"
+  name      = "azure_0din_actual"
   parent_id = "/subscriptions/e1cb04e4-3788-471a-881f-385e66ad80ab"
 
   body = {
@@ -42,9 +18,9 @@ resource "azapi_resource" "azure_zero_din_cost_export_actual" {
       compressionMode = "None"
       deliveryInfo = {
         destination = {
-          resourceId     = "/subscriptions/e1cb04e4-3788-471a-881f-385e66ad80ab/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
+          resourceId     = "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
           container      = "cost-management"
-          rootFolderPath = "azure_infrasec_daily_actual"
+          rootFolderPath = "azure_0din_daily_actual"
         }
       }
       partitionData = true
@@ -61,7 +37,7 @@ resource "azapi_resource" "azure_zero_din_cost_export_actual" {
 
 resource "azapi_resource" "azure_zero_din_cost_export_amortized" {
   type      = "Microsoft.CostManagement/exports@2025-03-01"
-  name      = "azure-infrasec_amortized"
+  name      = "azure_0din_amortized"
   parent_id = "/subscriptions/e1cb04e4-3788-471a-881f-385e66ad80ab"
 
   body = {
@@ -78,9 +54,9 @@ resource "azapi_resource" "azure_zero_din_cost_export_amortized" {
       compressionMode = "None"
       deliveryInfo = {
         destination = {
-          resourceId     = "/subscriptions/e1cb04e4-3788-471a-881f-385e66ad80ab/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
+          resourceId     = "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
           container      = "cost-management"
-          rootFolderPath = "azure_zero_din_daily_amortized"
+          rootFolderPath = "azure_0din_daily_amortized"
         }
       }
       partitionData = true
