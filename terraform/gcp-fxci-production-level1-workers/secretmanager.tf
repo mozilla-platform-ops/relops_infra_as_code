893461bae5a2b96c68db2b47e9568f69469ffe56
Jonathan Moss <jwmoss88@gmail.com>
2025-10-20 14:00:47 -0400

    add secret manager for cot

---
+++ terraform/gcp-fxci-production-level1-workers/secretmanager.tf
@@ -0,0 +1,26 @@
+resource "google_secret_manager_secret" "fake_cot" {
+  secret_id = "cot"
+  project   = "fxci-production-level1-workers"
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "fake_cot_worker_images" {
+  secret_id = google_secret_manager_secret.cot.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${module.google_deployment_accounts.service_account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "fake_cot_users" {
+  for_each = [
+    "aerickson",
+    "jmoss",
+    "mcornmesser",
+    "rcurran"
+  ]
+  project   = "fxci-production-level1-workers"
+  member    = "user:${each.value}@mozilla.com"
+  secret_id = google_secret_manager_secret.fake_cot.id
+  role      = "roles/secretmanager.secretAccessor"
+}
