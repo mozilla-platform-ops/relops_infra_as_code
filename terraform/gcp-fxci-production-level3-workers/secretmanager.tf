8d986e8ecc5834859440b137f36be4416ea774fd
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-10-21 12:28:42 -0400

    chore(cot): add secret manager for cot (#261)
    * add secret manager for cot
    
    * minor fixes

---
+++ terraform/gcp-fxci-production-level3-workers/secretmanager.tf
@@ -0,0 +1,26 @@
+resource "google_secret_manager_secret" "cot" {
+  secret_id = "cot"
+  project   = "fxci-production-level3-workers"
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_iam_member" "cot_worker_images" {
+  secret_id = google_secret_manager_secret.cot.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${module.google_deployment_accounts.service_account.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "cot_users" {
+  for_each = toset([
+    "aerickson",
+    "jmoss",
+    "mcornmesser",
+    "rcurran"
+  ])
+  project   = "fxci-production-level3-workers"
+  member    = "user:${each.value}@mozilla.com"
+  secret_id = google_secret_manager_secret.cot.id
+  role      = "roles/secretmanager.secretAccessor"
+}
