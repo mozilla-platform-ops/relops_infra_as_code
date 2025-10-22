65d604e0c9fac2bdc1d3aee43e57e6e86f14afad
Jonathan Moss <jwmoss88@gmail.com>
2025-10-22 10:10:38 -0400

    rename and add stuff

--- terraform/community-tc-workers/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  project = local.project
-  # not sure what these should be
-  region  = "us-west1"
-  zone    = "us-west1-b"
-}
--- terraform/community-tc-workers/terraform.tf
+++ terraform/community-tc-workers/terraform.tf
@@ -6,3 +6,11 @@ terraform {
     region         = "us-west-2"
   }
 }
+
+provider "google" {
+  project = local.project
+  # not sure what these should be
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
