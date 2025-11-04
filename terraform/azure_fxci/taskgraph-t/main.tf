c60ae07d657629191442c2f3e743b3480cbeebd2
Jonathan Moss <2729151+jwmoss@users.noreply.github.com>
2025-11-04 12:26:54 -0500

    [RELOPS-1989] add new taskgraph-t worker pool (#266)
    * add new taskgraph-t worker pool
    
    * shorten storage account name

---
+++ terraform/azure_fxci/taskgraph-t/main.tf
@@ -0,0 +1,49 @@
+locals {
+  location_map = {
+    "Canada Central"   = "canada-central"
+    "Central India"    = "central-india"
+    "Central US"       = "central-us"
+    "East US"          = "east-us"
+    "East US 2"        = "east-us-2"
+    "North Central US" = "north-central-us"
+    "North Europe"     = "north-europe"
+    "South India"      = "south-india"
+    "UK South"         = "uk-south"
+    "West US"          = "west-us"
+    "West US 2"        = "west-us-2"
+    "West US 3"        = "west-us-3"
+  }
+
+  config = yamldecode(file("../config.yaml"))
+  tags = {
+    terraform       = "true"
+    source_repo_url = "https://github.com/mozilla-platform-ops/relops_infra_as_code"
+  }
+  provisioner = "taskgraph-t"
+  provisioner_short = "taskt"
+}
+
+resource "azurerm_resource_group" "nongw" {
+  for_each = local.location_map
+  name     = "rg-${each.value}-${local.provisioner}"
+  location = each.key
+  tags     = local.tags
+}
+
+module "gecko-t_nogw" {
+  for_each                            = local.location_map
+  source                              = "../../azure_modules/workerPool_v1"
+  location                            = each.key
+  azurerm_virtual_network_name        = "vn-${each.value}-${local.provisioner}"
+  azurerm_subnet_name                 = "sn-${each.value}-${local.provisioner}"
+  resource_group_name                 = azurerm_resource_group.nongw[each.key].name
+  azurerm_network_security_group_name = "nsg-${each.value}-${local.provisioner}"
+  azurerm_storage_account_name        = "sa${each.value}-${local.provisioner_short}"
+  vnet_address_space                  = local.config.defaults.vnet_address_space
+  vnet_dns_servers                    = local.config.defaults.vnet_dns_servers
+  subnet_address_prefixes             = local.config.defaults.subnet_address_prefixes
+  provisioner                         = local.provisioner
+  tags                                = local.tags
+  nsg_security_rules                  = []
+  depends_on                          = [azurerm_resource_group.nongw]
+}
