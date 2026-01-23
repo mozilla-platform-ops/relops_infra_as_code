efdbb9e0660bd212ee04cb017df66e60e9ef87e7
Jonathan Moss <jmoss@mozilla.com>
2026-01-23 12:58:01 -0500

    add os-login enable

--- terraform/gcp-fxci-production-level1-workers/_compute_ssh_access.tf
+++ terraform/gcp-fxci-production-level1-workers/_compute_ssh_access.tf
@@ -1,5 +1,14 @@
 # grants SSH access to releng users via IAP tunnel and OS Login
 
+# enable OS Login at the project level
+# this allows users with roles/compute.osAdminLogin to SSH without needing
+# compute.instances.setMetadata permission (admins retain access via roles/owner)
+resource "google_compute_project_metadata_item" "enable_os_login" {
+  project = "fxci-production-level1-workers"
+  key     = "enable-oslogin"
+  value   = "TRUE"
+}
+
 # roles/iap.tunnelResourceAccessor - allows IAP tunnel connections
 resource "google_project_iam_member" "releng-users-iap-tunnel" {
   for_each = var.releng_users
