3690b500b649c7e597b1d1704a2ef62c3301a94c
Jonathan Moss <jmoss@mozilla.com>
2026-02-03 16:09:10 -0500

    add azure tc eng + update gitignore and lockfiles

---
+++ terraform/azure_tceng/finops.tf
@@ -0,0 +1,107 @@
+## Azure Cost Exports for Taskcluster Engineering Subscription
+
+import {
+  to = azapi_resource.taskcluster_cost_export_actual
+  id = "/subscriptions/8a205152-b25a-417f-a676-80465535a6c9/providers/Microsoft.CostManagement/exports/taskcluster-daily_actual"
+}
+
+import {
+  to = azapi_resource.taskcluster_cost_export_amortized
+  id = "/subscriptions/8a205152-b25a-417f-a676-80465535a6c9/providers/Microsoft.CostManagement/exports/taskcluster-daily_amortized"
+}
+
+resource "azapi_resource" "taskcluster_cost_export_actual" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "taskcluster-daily_actual"
+  parent_id = "/subscriptions/8a205152-b25a-417f-a676-80465535a6c9"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  body = {
+    properties = {
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "2024-09-03T00:00:00Z"
+          to   = "2050-02-01T00:00:00Z"
+        }
+      }
+      format                = "Csv"
+      compressionMode       = "gzip"
+      dataOverwriteBehavior = "OverwritePreviousReport"
+      exportDescription     = ""
+      deliveryInfo = {
+        destination = {
+          type           = "AzureBlob"
+          resourceId     = "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
+          container      = "cost-management"
+          rootFolderPath = "taskcluster"
+        }
+      }
+      partitionData = true
+      definition = {
+        type      = "ActualCost"
+        timeframe = "MonthToDate"
+        dataSet = {
+          granularity = "Daily"
+          configuration = {
+            columns     = []
+            dataVersion = "2021-10-01"
+            filters     = []
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "azapi_resource" "taskcluster_cost_export_amortized" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "taskcluster-daily_amortized"
+  parent_id = "/subscriptions/8a205152-b25a-417f-a676-80465535a6c9"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  body = {
+    properties = {
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "2024-09-03T00:00:00Z"
+          to   = "2050-02-01T00:00:00Z"
+        }
+      }
+      format                = "Csv"
+      compressionMode       = "gzip"
+      dataOverwriteBehavior = "OverwritePreviousReport"
+      exportDescription     = ""
+      deliveryInfo = {
+        destination = {
+          type           = "AzureBlob"
+          resourceId     = "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
+          container      = "cost-management"
+          rootFolderPath = "taskcluster"
+        }
+      }
+      partitionData = true
+      definition = {
+        type      = "AmortizedCost"
+        timeframe = "MonthToDate"
+        dataSet = {
+          granularity = "Daily"
+          configuration = {
+            columns     = []
+            dataVersion = "2021-10-01"
+            filters     = []
+          }
+        }
+      }
+    }
+  }
+}
