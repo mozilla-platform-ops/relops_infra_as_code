de1df1cafa3a03a94840e26ac539b543b5d43247
Mark Cornmesser <mcornmesser@mozilla.com>
2026-02-03 12:17:42 -0800

    replace infrasec finops tf

---
+++ terraform/azure_infrasec/finops.tf
@@ -0,0 +1,72 @@
+## Azure Cost Exports for Azure Infrastructure Security Subscription
+resource "azapi_resource" "infrasec_cost_export_actual" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "infrasec_actual"
+  parent_id = "/subscriptions/9b9774fb-67f1-45b7-830f-aafe07a94396"
+
+  body = {
+    properties = {
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "2025-09-02T00:00:00.000Z"
+          to   = "2050-02-01T00:00:00.000Z"
+        }
+      }
+      format = "Csv"
+      compressionMode = "None"
+      deliveryInfo = {
+        destination = {
+          resourceId     = "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
+          container      = "cost-management"
+          rootFolderPath = "infrasec"
+        }
+      }
+      partitionData = true
+      definition = {
+        type      = "ActualCost"
+        timeframe = "MonthToDate"
+        dataSet = {
+          granularity = "Daily"
+        }
+      }
+    }
+  }
+}
+
+resource "azapi_resource" "infrasec_cost_export_amortized" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "infrasec_amortized"
+  parent_id = "/subscriptions/9b9774fb-67f1-45b7-830f-aafe07a94396"
+
+  body = {
+    properties = {
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "2025-09-02T00:00:00.000Z"
+          to   = "2050-02-01T00:00:00.000Z"
+        }
+      }
+      format = "Csv"
+      compressionMode = "None"
+      deliveryInfo = {
+        destination = {
+          resourceId     = "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
+          container      = "cost-management"
+          rootFolderPath = "infrasec"
+        }
+      }
+      partitionData = true
+      definition = {
+        type      = "AmortizedCost"
+        timeframe = "MonthToDate"
+        dataSet = {
+          granularity = "Daily"
+        }
+      }
+    }
+  }
+}
