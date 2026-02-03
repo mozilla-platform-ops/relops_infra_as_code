4165dec38d36cff825d75bdac947cc4220e57f5f
Jonathan Moss <jmoss@mozilla.com>
2026-02-03 15:48:14 -0500

    add non fxci cost export

---
+++ terraform/azure_fx_nonci/finops.tf
@@ -0,0 +1,54 @@
+## Azure Cost Exports for Firefox Non-CI DevTest Subscription
+
+import {
+  to = azapi_resource.fx_nonci_cost_export_amortized
+  id = "/subscriptions/0a420ff9-bc77-4475-befc-a05071fc92ec/providers/Microsoft.CostManagement/exports/fx_non_ci-amortized-cost"
+}
+
+resource "azapi_resource" "fx_nonci_cost_export_amortized" {
+  type      = "Microsoft.CostManagement/exports@2025-03-01"
+  name      = "fx_non_ci-amortized-cost"
+  parent_id = "/subscriptions/0a420ff9-bc77-4475-befc-a05071fc92ec"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  body = {
+    properties = {
+      schedule = {
+        status     = "Active"
+        recurrence = "Daily"
+        recurrencePeriod = {
+          from = "2024-10-01T00:00:00Z"
+          to   = "2050-02-01T00:00:00Z"
+        }
+      }
+      format                = "Csv"
+      compressionMode       = "None"
+      dataOverwriteBehavior = "CreateNewReport"
+      exportDescription = ""
+      deliveryInfo = {
+        destination = {
+          type           = "AzureBlob"
+          resourceId     = "/subscriptions/108d46d5-fe9b-4850-9a7d-8c914aa6c1f0/resourceGroups/rg-azure-cost-mgmt/providers/Microsoft.Storage/storageAccounts/safinopsdata"
+          container      = "cost-management"
+          rootFolderPath = "fx_non_ci_daily_amortized"
+        }
+      }
+      partitionData = true
+      definition = {
+        type      = "AmortizedCost"
+        timeframe = "MonthToDate"
+        dataSet = {
+          granularity = "Daily"
+          configuration = {
+            columns     = []
+            dataVersion = "2021-10-01"
+            filters     = []
+          }
+        }
+      }
+    }
+  }
+}
