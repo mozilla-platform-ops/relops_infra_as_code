94f72f4e84fec3fcce29922f1dd481ce294f5fac
jwmossmoz <jmoss@mozilla.com>
2026-02-19 12:37:12 -0500

    RELOPS-2209: add Azure resource group for Kitchen Windows tests (#285)
    * RELOPS-2209: replace puppet kitchen identity with ronin OIDC
    
    * Ignore and untrack terraform lockfile in azure_ad
    
    * Migrate azure_ad module config to azuread provider v3
    
    * add rg for puppet
    
    * add in existing file
    
    * fix tags

--- terraform/azure_fxci/resource_groups.tf
+++ terraform/azure_fxci/resource_groups.tf
@@ -19,3 +19,19 @@ resource "azurerm_resource_group" "windows7" {
     })
   )
 }
+
+resource "azurerm_resource_group" "ronin_puppet_test_kitchen" {
+  name     = "ronin-puppet-test-kitchen"
+  location = "East US 2"
+
+  tags = {
+    "Name"             = "ronin-puppet-test-kitchen"
+    "owner"            = "relops@mozilla.com"
+    "owner_email"      = "mcornmesser@mozilla.com"
+    "production_state" = "production"
+    "project_name"     = "azure_fxci"
+    "provisioner"      = "test-kitchen-azurerm"
+    "source_repo_url"  = "https://github.com/mozilla-platform-ops/relops_infra_as_code"
+    "terraform"        = "true"
+  }
+}
