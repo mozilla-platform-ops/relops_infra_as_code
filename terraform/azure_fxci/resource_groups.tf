d181c334cba94ad92ab260db578dd2cc40cffb89
Jonathan Moss <jmoss@mozilla.com>
2026-02-19 12:30:04 -0500

    fix tags

--- terraform/azure_fxci/resource_groups.tf
+++ terraform/azure_fxci/resource_groups.tf
@@ -24,12 +24,14 @@ resource "azurerm_resource_group" "ronin_puppet_test_kitchen" {
   name     = "ronin-puppet-test-kitchen"
   location = "East US 2"
 
-  tags = merge(local.common_tags,
-    tomap({
-      "Name"        = "ronin-puppet-test-kitchen"
-      "owner"       = "relops@mozilla.com"
-      "provisioner" = "test-kitchen-azurerm"
-    })
-  )
+  tags = {
+    "Name"             = "ronin-puppet-test-kitchen"
+    "owner"            = "relops@mozilla.com"
+    "owner_email"      = "mcornmesser@mozilla.com"
+    "production_state" = "production"
+    "project_name"     = "azure_fxci"
+    "provisioner"      = "test-kitchen-azurerm"
+    "source_repo_url"  = "https://github.com/mozilla-platform-ops/relops_infra_as_code"
+    "terraform"        = "true"
+  }
 }
-
