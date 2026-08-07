c7277942523bf55b15591864447af19527a8c7f1
Jonathan Moss <jmoss@mozilla.com>
2026-08-07 13:46:55 -0400

    RELOPS-2495: remove individual WNS owner

--- terraform/azure_ad/sp_firefox_wns.tf
+++ terraform/azure_ad/sp_firefox_wns.tf
@@ -1,27 +1,16 @@
 # Mozilla-owned identity for the Firefox WNS work in Bug 1803416.
 # Add a client credential only after the sender and secret store are defined.
 
-data "azuread_user" "aborondo" {
-  user_principal_name = "aborondo@mozilla.com"
-}
-
-locals {
-  firefox_wns_owners = concat(
-    data.azuread_group.relops.members,
-    [data.azuread_user.aborondo.object_id],
-  )
-}
-
 resource "azuread_application" "firefox_wns" {
   display_name     = "Firefox WNS"
   description      = "Mozilla-owned identity for Firefox Windows App SDK push notification work (RELOPS-2495, Bug 1803416)."
   sign_in_audience = "AzureADMultipleOrgs"
-  owners           = local.firefox_wns_owners
+  owners           = data.azuread_group.relops.members
 }
 
 resource "azuread_service_principal" "firefox_wns" {
   client_id = azuread_application.firefox_wns.client_id
-  owners    = local.firefox_wns_owners
+  owners    = data.azuread_group.relops.members
   tags      = concat(["name:firefox-wns"], local.sp_tags)
 }
 
