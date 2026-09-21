0ad7ffefe1f53fa591ec2a6c7eb23fc631eaa7b7
markcor <mcornmesser@mozilla.com>
2026-09-21 10:22:00 -0700

    RELOPS-2548: Add FooFrix Windows 11 25H2 image (#345)
    * Add FooFrix image build resource group in West US 3
    
    * Add FooFrix Windows 11 25H2 image definition
    
    * Remove unused FooFrix West US 3 build group
    
    * Use a dedicated gallery for FooFrix 25H2
    
    * RELOPS-2548: Share FooFrix image resource definitions
    
    * Revert "RELOPS-2548: Share FooFrix image resource definitions"
    
    This reverts commit 06bd8e3ae633c0cb1e4a5827d71e725777be6637.
    
    ---------
    
    Co-authored-by: Jonathan Moss <jmoss@mozilla.com>

--- terraform/azure_foofrix/outputs.tf
+++ terraform/azure_foofrix/outputs.tf
@@ -38,6 +38,18 @@ output "windows_image_definition_id" {
   value = azurerm_shared_image.windows.id
 }
 
+output "windows_25h2_image_definition_id" {
+  value = azurerm_shared_image.windows_25h2.id
+}
+
+output "windows_25h2_image_gallery_id" {
+  value = azurerm_shared_image_gallery.windows_25h2.id
+}
+
+output "windows_25h2_image_gallery_name" {
+  value = azurerm_shared_image_gallery.windows_25h2.name
+}
+
 output "image_build_client_id" {
   value = data.azuread_service_principal.foofrix_image_build.client_id
 }
